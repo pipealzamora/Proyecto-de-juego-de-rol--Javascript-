cf425fc56144b3520e21a811230b6ad167158770
pipeaalzamora <pipeaalzamora@gmail.com>
2023-12-21 10:49:01 -0300

    Se agrego codigo al script

--- script.js
+++ script.js
@@ -18,4 +18,21 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText= document.querySelector("#monsterHealth")
 
 button1.onclick= goStore;
-button2.onclick= 
+button2.onclick= goCave;
+button3.onclick= fightDragon;
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innetText= "You enter the store.";
+  }
+function goCave() {
+    console.log("Going to cave.")
+}
+function fightDragon(){
+    console.log("Fighting dragon.")
+}
